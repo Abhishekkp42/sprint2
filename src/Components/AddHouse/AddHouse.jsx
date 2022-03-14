d1003c7667e08934e8f72d32a27e8feb1d0e489c
Abhishekkp42 <96106223+Abhishekkp42@users.noreply.github.com>
2022-03-14 09:58:23 +0530

    components created

--- src/Components/AddHouse/AddHouse.jsx
+++ src/Components/AddHouse/AddHouse.jsx
@@ -1,32 +1,54 @@
+import {useState} from "react";
+
+
 export const AddHouse = () => {
+
+  const [text, setText] = useState()
+
   return (
     <div className="addHouseContainer">
       <form>
         <label>name</label>
-        <input type="text" className="name" value={""} required />
+        <input type="text" className="name" value={""} required onChange={(e) => {
+          setText(e.target.value)
+        }}/>
         <br />
         <label>ownerName</label>
-        <input value={""} type="text" className="ownerName" required />
+        <input value={""} type="text" className="ownerName" required onChange={(e) => {
+          setText(e.target.value)
+        }}/>
         <br />
         <label>address</label>
-        <input value={""} type="text" className="address" required />
+        <input value={""} type="text" className="address" required onChange={(e) => {
+          setText(e.target.value)
+        }}/>
         <br />
         <label>areaCode</label>
-        <input value={""} type="text" className="areaCode" required />
+        <input value={""} type="text" className="areaCode" required onChange={(e) => {
+          setText(e.target.value)
+        }}/>
         <br />
         <label>rent</label>
-        <input value={""} type="text" className="rent" required />
+        <input value={""} type="text" className="rent" required onChange={(e) => {
+          setText(e.target.value)
+        }}/>
         <br />
         <label>preferredTenant</label>
         <br />
         <label>bachelor</label>
-        <input checked={""} type="checkbox" className="bachelor" />
+        <input checked={""} type="checkbox" className="bachelor" onChecked={(e) => {
+          setText(e.target.value)
+        }}/>
         <br />
         <label>married</label>
-        <input checked={""} type="checkbox" className="married" />
+        <input checked={""} type="checkbox" className="married" onChecked={(e) => {
+          setText(e.target.value)
+        }}/>
         <br />
         <label>image</label>
-        <input value={""} type="text" className="image" required />
+        <input value={""} type="text" className="image" required onChange={(e) => {
+          setText(e.target.value)
+        }}/>
         <br />
         <input className="submitBtn" type="submit" />
       </form>
